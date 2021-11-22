a893e86df7024bc2ad54b6f088ac17c496ad4f86
Sunil Kumar <sdkumar@hmc.edu>
2021-11-22 10:52:36 -0800

    fixing

--- train_resnet.py
+++
@@ -1,9 +0,0 @@
-from resnet import ResNet50
-import os
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-import torchvision
-from torchvision import datasets, transforms
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True)
