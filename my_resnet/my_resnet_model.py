5b729027b5b71eccb7a38e9d66190b7e55a957b5
Sunil Kumar <sdkumar@hmc.edu>
2021-12-10 22:35:46 -0800

    done with project, updating repo

--- my_resnet/my_resnet_model.py
+++ my_resnet/my_resnet_model.py
@@ -1,7 +1,7 @@
 import os
 import torch
 from torch import nn
-from torch._C import InferredType
+
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
@@ -13,7 +13,7 @@ class ResNet50(nn.Module):
         #declare layers in the network
         #upsample image by 7 from bsx3x32x32 to bsx3x224x224
         self.upsample = nn.Upsample(scale_factor=7)
-        #we are using an untrained resnet18, so load it in
+        #we are using an untrained resnet50, so load it in
         self.resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=False)
         #we are classifying on cifar10 which has 10 output classes not 1000, so fix this
         self.resnet.fc = nn.Linear(in_features=2048, out_features=10)
